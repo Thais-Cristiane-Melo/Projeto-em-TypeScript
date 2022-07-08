3d4a6f295b20686c4f814de5d02b63ec7ffc0cb0
Thais Melo <102183748+Thais-Cristiane-Melo@users.noreply.github.com>
2022-07-08 13:48:10 -0300

    Add files via upload

---
+++ desafio2.js
@@ -0,0 +1,23 @@
+// Como podemos melhorar o esse código usando TS? 
+
+let pessoa1 = {};
+pessoa1.nome = "Sonia";
+pessoa1.idade = 60;
+pessoa1.profissao = "Dona de casa"
+
+let pessoa2 = {}
+pessoa2.nome = "Mauro";
+pessoa2.idade = 50;
+pessoa2.profissao = "Programador";
+
+let pessoa3 = {
+    nome: "Iara",
+    idade: "41",
+    profissao: "Vendedora"
+};
+
+let pessoa4 = {
+    nome : "Junior",
+    idade : 40,
+    profissao : "Motorista"
+}
